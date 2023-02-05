12e6edddf86ada75ae7cf8292ae66534b4a3c750
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2023-02-05 22:49:57 +0100

    update premake5 with missing files

--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,7 @@ project "glfw"
 		"include/GLFW/glfw3native.h",
 		"src/internal.h",
 		"src/platform.h",
+		"src/platform.c",
 		"src/mappings.h",
 		"src/context.c",
 		"src/init.c",
@@ -23,6 +24,7 @@ project "glfw"
 		"src/osmesa_context.c",
 		"src/null_platform.h",
 		"src/null_joystick.h",
+		"src/null_joystick.c",
 		"src/null_init.c",
 
 		"src/null_monitor.c",
@@ -63,15 +65,22 @@ project "glfw"
 		{
 			"src/win32_init.c",
 			"src/win32_module.c",
+
 			"src/win32_joystick.c",
+			"src/win32_joystick.h",
+
 			"src/win32_monitor.c",
+			"src/win32_platform.h",
+
 			"src/win32_time.h",
 			"src/win32_time.c",
+
 			"src/win32_thread.h",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
+			
 			"src/win32_window.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
